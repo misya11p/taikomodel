19246d14b752a718285dd866b3d21059dbdc9d96
misya11p <90894519+misya11p@users.noreply.github.com>
2026-03-12 20:41:27 +0900

    add: overwrite option

--- preprocess.py
+++ preprocess.py
@@ -16,15 +16,24 @@ app = typer.Typer(add_completion=False, context_settings=CONTEXT_SETTINGS)
 
 
 @app.command()
-def main():
+def main(
+    overwrite: bool = typer.Option(
+        False,
+        "--overwrite", "-o",
+        help="Whether to overwrite existing preprocessed images.",
+    )
+):
     dpath_images = Path(DPATH_IMAGES)
     dpath_preprocessed = Path(DPATH_PREPROCESSED)
     dpath_preprocessed.mkdir(exist_ok=True)
     fpaths = list(dpath_images.glob("*.jpg"))
     for fpath in tqdm(fpaths):
+        fpath_out = dpath_preprocessed / fpath.name
+        if (not overwrite) and fpath_out.exists():
+            continue
         img = Image.open(fpath)
         img = preprocess(img)
-        img.save(dpath_preprocessed / fpath.name)
+        img.save(fpath_out)
 
 
 def preprocess(img):
