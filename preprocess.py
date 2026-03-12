75259c8cc72cb5fe72a8abb7c16a073d8aec67b6
misya11p <90894519+misya11p@users.noreply.github.com>
2026-03-12 18:32:42 +0900

    preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+import numpy as np
+from PIL import Image
+import typer
+from tqdm import tqdm
+
+
+W, H = 1280, 720
+DPATH_IMAGES = "data/images/"
+DPATH_PREPROCESSED = "data/preprocessed/"
+
+
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+app = typer.Typer(add_completion=False, context_settings=CONTEXT_SETTINGS)
+
+
+@app.command()
+def main():
+    dpath_images = Path(DPATH_IMAGES)
+    dpath_preprocessed = Path(DPATH_PREPROCESSED)
+    dpath_preprocessed.mkdir(exist_ok=True)
+    fpaths = list(dpath_images.glob("*.jpg"))
+    for fpath in tqdm(fpaths):
+        img = Image.open(fpath)
+        img = preprocess(img)
+        img.save(dpath_preprocessed / fpath.name)
+
+
+def preprocess(img):
+    img = img.resize((W, H))
+    imarr = np.array(img)
+    imarr = imarr[:-220, 470:-40]
+    imarr = np.concatenate((imarr[:90], imarr[280:]), axis=0)
+    imarr[70:95] = 0
+    imarr[130:160] = 0
+    imarr[95:105, :492] = 0
+    imarr[120:130, 492:] = 0
+    imarr[95:120, 635:] = 0
+    imarr[242:290, 15:250] = 0
+    img = Image.fromarray(imarr)
+    return img
+
+
+if __name__ == "__main__":
+    app()
