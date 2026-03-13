3fc5b4415e104b993f052ee9ad0217b7f412cbb9
misya11p <90894519+misya11p@users.noreply.github.com>
2026-03-13 11:00:34 +0900

    refactor

--- infer.py
+++ infer.py
@@ -58,7 +58,6 @@ async def run(model, instruction, client, images):
         json.dump(results, f, ensure_ascii=False, indent=2)
 
 
-
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
